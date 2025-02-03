b6b67b0bfec57357ff746d03940f2e06ec27ab8e
Baravik Veranika <borovikveronika@gmail.com>
2025-02-03 06:09:44 +0100

    feat: ts

--- src/customCache.ts
+++ src/customCache.ts
@@ -1,3 +1,49 @@
 export default class CustomCache<K, V> {
-  // your code here
+  private cache: Map<K, V>;
+  private accessOrder: K[];
+  private maxSize: number;
+
+  constructor(maxSize: number) {
+    this.maxSize = maxSize;
+    this.cache = new Map<K, V>;
+    this.accessOrder = [];
+  }
+  set(key: K, value: V): void {
+    if (this.cache.has(key)) {
+      const index = this.accessOrder.indexOf(key);
+      if (index !== -1) {
+        this.accessOrder.splice(index, 1);
+      }
+    }
+    else if (this.cache.size >= this.maxSize && this.accessOrder.length > 0) {
+      const oldestKey = this.accessOrder[0];
+      if (oldestKey !== undefined) {
+        this.cache.delete(oldestKey);
+        this.accessOrder.shift();
+      }
+    }
+    this.cache.set(key, value);
+    this.accessOrder.push(key);
+  }
+  get(key: K): V | undefined {
+    const value = this.cache.get(key);
+    
+    if (value !== undefined) {
+      const index = this.accessOrder.indexOf(key);
+      if (index !== -1) {
+        this.accessOrder.splice(index, 1);
+        this.accessOrder.push(key);
+      }
+    }
+    return value;
+  }
+
+  size(): number {
+    return this.cache.size;
+  }
+
+  clear(): void {
+    this.cache.clear();
+    this.accessOrder = [];
+  }
 }
